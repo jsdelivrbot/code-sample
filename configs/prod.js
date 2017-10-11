07adb1cdc50097d3b595e7402273922dec09e706
Viet Tran <viet@prosperworks.com>
2017-10-11 13:09:10 -0700

    remove sub folder

--- configs/prod.js
+++ configs/prod.js
@@ -1,6 +1,6 @@
 import { PROD } from 'constants/envConstants'
 
-export const ROUTER_ROOT = '/v2'
+export const ROUTER_ROOT = ''
 export const API_PROTOCOL = 'https://'
 export const API_HOST = 'foo.com'
 export const API_PREFIX = API_PROTOCOL + API_HOST + '/api'
